74af6115758cd7599f6b3e076dda1e48776da3a9
Mirko R <mirko_richter@me.com>
2021-12-12 09:39:05 +0100

    fix: handle non-existing thumbnail, authors or title

--- src/Book.js
+++ src/Book.js
@@ -4,16 +4,24 @@ function Book (props) {
 
     const {book, shelves, onBookMove} = props;
 
+    const thumbnail = 'imageLinks' in book 
+        && 'thumbnail' in book.imageLinks ?
+        book.imageLinks.thumbnail: '';
+    const title = 'title' in book ? book.title: '';
+    const authors = 'authors' in book && book.authors.length > 0? book.authors: '';
+
     return (
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                <div className="book-cover"
+                    style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})`}}
+                ></div>
                 <div className="book-shelf-changer">
                     <Move shelves={shelves} book={book} onBookMove={onBookMove}/>
                 </div>
             </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors !== undefined && book.authors.toString()}</div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors.toString()}</div>
         </div>
     )
 }
